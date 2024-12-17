dacd9c9f77b06199218bfc2bac03aaaea6be6f25
Daniel Aguiar <daniel_aguiar_alves@hotmail.com>
2024-12-17 03:15:48 -0300

    fix: lambda security group

--- main.tf
+++ main.tf
@@ -7,6 +7,35 @@ provider "aws" {
   region = module.global_variables.aws_region
 }
 
+<<<<<<< Updated upstream
+=======
+data "terraform_remote_state" "database_state" {
+  backend = "s3"
+
+  config = {
+    bucket = "cloud-burger-states"
+    key    = "prod/database.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "terraform_remote_state" "eks_state" {
+  backend = "s3"
+
+  config = {
+    bucket = "cloud-burger-states"
+    key    = "prod/eks.tfstate"
+    region = "us-east-1"
+  }
+}
+
+locals {
+  aws_vpc_id          = data.terraform_remote_state.eks_state.vpc_id
+  aws_private_subnets = data.terraform_remote_state.eks_state.outputs.private_subnets
+  rds_public_sg_id    = data.terraform_remote_state.database_state.rds_public_sg_id
+}
+
+>>>>>>> Stashed changes
 terraform {
   backend "s3" {
     bucket = "cloud-burger-states"
