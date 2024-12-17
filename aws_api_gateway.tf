da0e345df264e2027602e6538a739ef01a48b768
ELuzzi <dudaluzzi@gmail.com>
2024-12-16 22:54:02 -0300

    feat: Insere openAPI com authorizers

--- aws_api_gateway.tf
+++ aws_api_gateway.tf
@@ -1,6 +1,13 @@
 resource "aws_api_gateway_rest_api" "main" {
   name = "${var.project}-main-${var.environment}"
 
+  body = templatefile("${path.module}/openapi.yaml", {
+    load_balancer_uri = local.eks_cluster_endpoint,
+    authorizer_uri = module.lambda_authorizer.invoke_arn,
+    authorizer_credentials = aws_iam_role.invocation_role.arn,
+    provider_arn = aws_cognito_user_pool.main.arn
+  })
+
   endpoint_configuration {
     types = ["REGIONAL"]
   }
@@ -22,17 +29,10 @@ resource "aws_api_gateway_deployment" "main" {
   rest_api_id = aws_api_gateway_rest_api.main.id
 
   triggers = {
-    redeployment = sha1(jsonencode([aws_api_gateway_rest_api.main]))
+    redeployment = sha1(jsonencode([aws_api_gateway_rest_api.main.body]))
   }
 
   lifecycle {
     create_before_destroy = true
   }
-}
-
-resource "aws_api_gateway_authorizer" "main" {
-  name                   = "${var.project}-main-${var.environment}"
-  rest_api_id            = aws_api_gateway_rest_api.main.id
-  authorizer_uri         = module.lambda_authorizer.invoke_arn
-  authorizer_credentials = aws_iam_role.invocation_role.arn
 }
