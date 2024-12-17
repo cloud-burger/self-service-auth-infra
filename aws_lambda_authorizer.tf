0f6925741deae44876c9c7cd2411e6965f856bdf
Daniel Alves <daniel_aguiar_alves@hotmail.com>
2024-12-17 03:18:26 -0300

    fix: lambda security group (#12)

--- aws_lambda_authorizer.tf
+++ aws_lambda_authorizer.tf
@@ -1,13 +1,14 @@
 module "lambda_authorizer" {
-  source           = "./modules/lambda"
-  name             = "${var.project}-authorizer"
-  lambda_role      = aws_iam_role.lambda_role.arn
-  handler          = "src/cmd/functions/authorizer/index.handler"
-  source_bucket    = module.global_variables.source_bucket
-  source_key       = "authorizer.zip"
-  project          = var.project
-  subnet_ids       = local.aws_private_subnets
-  source_code_hash = base64encode(sha256("${var.commit_hash}"))
+  source             = "./modules/lambda"
+  name               = "${var.project}-authorizer"
+  lambda_role        = aws_iam_role.lambda_role.arn
+  handler            = "src/cmd/functions/authorizer/index.handler"
+  source_bucket      = module.global_variables.source_bucket
+  source_key         = "authorizer.zip"
+  project            = var.project
+  subnet_ids         = local.aws_private_subnets
+  security_group_ids = [aws_security_group.lambda_auth_sg.id]
+  source_code_hash   = base64encode(sha256("${var.commit_hash}"))
 
   environment_variables = {
     DATABASE_USERNAME           = data.aws_ssm_parameter.database_username.value
--- main.tf
+++ main.tf
@@ -7,6 +7,16 @@ provider "aws" {
   region = module.global_variables.aws_region
 }
 
+data "terraform_remote_state" "database_state" {
+  backend = "s3"
+
+  config = {
+    bucket = "cloud-burger-states"
+    key    = "prod/database.tfstate"
+    region = "us-east-1"
+  }
+}
+
 data "terraform_remote_state" "eks_state" {
   backend = "s3"
 
@@ -18,7 +28,9 @@ data "terraform_remote_state" "eks_state" {
 }
 
 locals {
+  aws_vpc_id          = data.terraform_remote_state.eks_state.vpc_id
   aws_private_subnets = data.terraform_remote_state.eks_state.outputs.private_subnets
+  rds_public_sg_id    = data.terraform_remote_state.database_state.rds_public_sg_id
 }
 
 terraform {
